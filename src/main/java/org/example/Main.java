907699aa232cfade1ef96b6d78d44126caeb3138
irtassedat <sedatirtas.1@gmail.com>
2024-09-05 03:51:04 +0300

    solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,49 @@
 package org.example;
 
 
+import org.example.entity.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee(0, "Elton", "John"));
+        employees.add(new Employee(1, "Micheal", "Scott"));
+        employees.add(new Employee(2, "Nelson", "Mandela"));
+        employees.add(new Employee(3, "Ryan", "Gosling"));
+        employees.add(new Employee(4, "Elvis", "Stojko"));
+        employees.add(new Employee(5, "Lightning", "McQueen"));
+        employees.add(new Employee(5, "Lightning", "McQueen"));
+
+        System.out.println(findUniques(employees));
+        System.out.println(findDuplicates(employees));
+        System.out.println(removeDuplicates(employees));
+        System.out.println(employees);
+        System.out.println(WordCounter.calculatedWord());
+    }
+
+    public static List<Employee> findDuplicates(List<Employee> list){
+        List<Employee> nonDup = new ArrayList<>();
+        List<Employee> dup = new ArrayList<>();
+        for(Employee emp : list){
+            if(nonDup.contains(emp)) dup.add(emp);
+            else nonDup.add(emp);
+        }
+        return dup;
+    }
+
+    public static Map<Integer, Employee> findUniques(List<Employee> list){
+        Map<Integer, Employee> employees= new HashMap<>();
+        for(Employee emp : list){
+            if(emp!=null) employees.put(emp.getId(), emp);
+        }
+        return employees;
+    }
+
+    public static List<Employee> removeDuplicates(List<Employee> list){
+        List<Employee> dummy = new ArrayList<>(list);
+        dummy.removeAll(findDuplicates(list));
+        return dummy;
     }
 }
---
+++ src/main/java/org/example/WordCounter.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.*;
+
+public class WordCounter {
+
+    static String str = "When the offensive resumed, the Turks received their first victory when " +
+            "the Greeks encountered stiff resistance in the battles of First and Second İnönü," + " due to İsmet Pasha's organization of an irregular militia into a regular army. " + " The two victories led to Allied proposals to amend the Treaty of Sèvres where both Ankara and Istanbul were represented, but Greece refused." + " With the conclusion of the Southern and Eastern fronts, Ankara was able to concentrate more forces on the West against the Greeks." + " They also began to receive support from Soviet Union, as well as France and Italy, who sought to check British influence in the Near East.\n" + " June–July 1921 saw heavy fighting in the Battle of Kütahya-Eskişehir. While it was an eventual Greek victory, the Turkish army withdrew in good order to the Sakarya river, their last line of defence." + " Mustafa Kemal Pasha replaced İsmet Pasha after the defeat as commander in chief as well as his political duties." + " The decision was made in the Greek military command to march on the nationalist capital of Ankara to force Mustafa Kemal to the negotiating table." + " For 21 days, the Turks and Greeks fought a pitched battle at the Sakarya river, which ended in Greek withdrawal." + " Almost of year of stalemate without much fighting followed, during which Greek moral and discipline faltered while Turkish strength increased." + " French and Italian forces evacuated from Anatolia. The Allies offered an armistice to the Turks, which Mustafa Kemal refused.";
+
+    public static Map<String, Integer> calculatedWord() {
+        HashMap<String, Integer> wordsMap = new HashMap<>();
+
+        str = str.replaceAll("[^a-zA-Z\\s]", "").toLowerCase();
+
+        for (String word : str.split(" ")) {
+            //Reference to a static method
+            wordsMap.merge(word, 1, Integer::sum);
+
+            //Lambda Expression
+//            wordsMap.merge(word, 1, (oldValue, newValue) -> oldValue + newValue);
+
+            //The first code I wrote
+//            if (wordsMap.get(word) != null) {
+//                wordsMap.put(word, wordsMap.get(word) + 1);
+//            } else {
+//                wordsMap.put(word, 1);
+//            }
+        }
+
+        return wordsMap;
+    }
+}
